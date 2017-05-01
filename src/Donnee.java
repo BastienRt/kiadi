c9f9b994dadf4be2d4d160466f28022eb85bda2f
Bastien <bastien.rt@gmail.com>
2017-05-01 16:33:57 +0200

    First Commit

---
+++ src/Donnee.java
@@ -0,0 +1,9 @@
+
+
+public interface Donnee {
+	
+	String consumerKey = "YWEiRrA8q310zI4uT3wdpqaA3";
+	String consumerKeySecret = "hHajoyNSdJzVQxYvdBopEMndsT8MEJbfOOJjZfv3866Tp7OJzn";
+	String accessToken = "333884789234-44phppnv1k7m4oh0cctr9st19m79g9b4";
+	String accessTokenSecret = "571qb4p8guopF-Tx_4BP9M0P";
+}
